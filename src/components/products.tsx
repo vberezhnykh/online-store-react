6b52fedf6fb90f9f0bacdb4979120c3d5a16c50f
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2023-06-27 21:27:33 +0300

    feat: add header to products component on main page

--- src/components/products.tsx
+++ src/components/products.tsx
@@ -1,17 +1,58 @@
-import { Box, MenuItem, Select, TextField } from "@mui/material";
+import {
+  Box,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  TextField,
+} from "@mui/material";
+import { useState } from "react";
 
 const Products = () => {
+  const [age, setAge] = useState("");
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value);
+  };
+
   return (
-    <Box content="section" sx={{ border: 1, width: "70%" }}>
-      <Box content="header">
-        <TextField id="select" label="Age" select>
-          <MenuItem>Sort by price ASC</MenuItem>
-          <MenuItem>Sort by price DESC</MenuItem>
-          <MenuItem>Sort by rating ASC</MenuItem>
-          <MenuItem>Sort by rating DESC</MenuItem>
-          <MenuItem>Sort by discount ASC</MenuItem>
-          <MenuItem>Sort by discount DESC</MenuItem>
-        </TextField>
+    <Box content="section" sx={{ border: 1, width: "70%", padding: 1 }}>
+      <Box
+        content="header"
+        sx={{
+          border: 1,
+          borderRadius: 3,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          padding: 1,
+        }}
+      >
+        <FormControl sx={{ m: 1, minWidth: 200 }} size="small">
+          <InputLabel id="demo-select-small-label">Sort by options</InputLabel>
+          <Select
+            labelId="demo-select-small-label"
+            id="demo-select-small"
+            value={age}
+            label="Age"
+            onChange={handleChange}
+          >
+            <MenuItem value={"PRICE_ASC"}>Sort by price ASC</MenuItem>
+            <MenuItem value={"PRICE_DESC"}>Sort by price DESC</MenuItem>
+            <MenuItem value={"RATING_ASC"}>Sort by rating ASC</MenuItem>
+            <MenuItem value={"RATING_DESC"}>Sort by rating DESC</MenuItem>
+            <MenuItem value={"DISCOUNT_ASC"}>Sort by discount ASC</MenuItem>
+            <MenuItem value={"SICOUNT_DESC"}>Sort by discount DESC</MenuItem>
+          </Select>
+        </FormControl>
+        <span>Found: 100</span>
+        <TextField
+          id="outlined-search"
+          label="Search field"
+          type="search"
+          size="small"
+        />
       </Box>
     </Box>
   );
